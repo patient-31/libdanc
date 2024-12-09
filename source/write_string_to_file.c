1fb73bbaf9f997b17ef4a17ad90318dfe348fb54
Daniel Haddington <haddingtondaniel5@icloud.com>
2024-12-10 07:34:57 +1030

    ...

---
+++ source/write_string_to_file.c
@@ -0,0 +1,14 @@
+/*
+ * ________________________________________________________________________________
+ * |  File: write_string_to_file.c
+ * |  Project: source
+ * |  File Created: Tuesday, 10th December 2024 07:32 am
+ * |  Author: Daniel Haddington [haddingtondaniel5@icloud.com]
+ */
+
+#include "../includes/libdanc.h"
+
+// void write_string_to_file(const char *filename, const char *content)
+// {
+
+// }
